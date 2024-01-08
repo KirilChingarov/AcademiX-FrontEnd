91ba457381d68c3a27cf327f9a4479d9677f3a53
Kiril Chingarov <kiril.chingarov@gmail.com>
2024-01-08 20:04:28 +0200

    Added Reviewers related pages

---
+++ src/pages/ViewReviewer/ViewReviewer.js
@@ -0,0 +1,29 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import './ViewReviewer.css'
+
+const ViewReviewer = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const reviewer = location.state.reviewer
+
+  const handleBack = () => {
+    navigate(-1)
+  }
+
+  return (
+    <div className='viewReviewer'>
+      <h1>View Project Details</h1>
+      <div className='details'>
+        <h1>{reviewer.firstName} {reviewer.lastName}</h1>
+        <p>Email: {reviewer.email}</p>
+        <p>Cabinet: {reviewer.cabinet}</p>
+        <p>Working Time:  {reviewer.workingTime}</p>
+      </div>
+      <button onClick={handleBack} className='backButton'>
+        Go back
+      </button>
+    </div>
+  )
+}
+
+export default ViewReviewer
